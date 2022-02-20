cea7b0e9886d8fdc74add372fe385ca8d40529ab
Tom H Anderson <tom.h.anderson@gmail.com>
2022-02-20 16:59:58 -0700

    Added doctrine orm

---
+++ cli-config.php
@@ -0,0 +1,21 @@
+<?php
+use Doctrine\ORM\Tools\Console\ConsoleRunner;
+use Doctrine\ORM\Tools\Setup;
+use Doctrine\ORM\EntityManager;
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+// Create a simple "default" Doctrine ORM configuration for Annotations
+$config = Setup::createXMLMetadataConfiguration(array(__DIR__ . "/test/config/orm"), true);
+
+// database configuration parameters
+$conn = array(
+    'driver' => 'pdo_sqlite',
+    'dbname' => ':memory:',
+);
+
+// obtaining the entity manager
+$entityManager = EntityManager::create($conn, $config);
+
+return ConsoleRunner::createHelperSet($entityManager);
+
