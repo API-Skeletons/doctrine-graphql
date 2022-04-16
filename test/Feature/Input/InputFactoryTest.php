3ea83f168ef8b33c67326b8aafa1899c73f46cf1
Tom H Anderson <tom.h.anderson@gmail.com>
2022-04-16 14:45:52 -0600

    Input test for optional and required

--- test/Feature/Input/InputFactoryTest.php
+++ test/Feature/Input/InputFactoryTest.php
@@ -13,7 +13,7 @@ use GraphQL\Type\Schema;
 
 class InputFactoryTest extends AbstractTest
 {
-    public function testCreateMutationInput(): void
+    public function testInputWithRequiredField(): void
     {
         $config = new Config([
             'group' => 'InputFactoryTest',
@@ -47,6 +47,108 @@ class InputFactoryTest extends AbstractTest
 
         $query = 'mutation {
             testInput(id: 1, input: { name: "inputTest" }) {
+                id
+                name
+            }
+        }';
+
+        $result = GraphQL::executeQuery($schema, $query);
+        $output = $result->toArray();
+
+        $this->getEntityManager()->clear();
+        $user = $this->getEntityManager()->getRepository(User::class)
+            ->find(1);
+
+        $this->assertEquals('inputTest', $user->getName());
+        $this->assertEquals(1, $output['data']['testInput']['id']);
+        $this->assertEquals('inputTest', $output['data']['testInput']['name']);
+    }
+
+    public function testInputWithOptionalField(): void
+    {
+        $config = new Config([
+            'group' => 'InputFactoryTest',
+        ]);
+
+        $driver = new Driver($this->getEntityManager(), $config);
+
+        $schema = new Schema([
+            'mutation' => new ObjectType([
+                'name' => 'mutation',
+                'fields' => [
+                    'testInput' => [
+                        'type' => $driver->type(User::class),
+                        'args' => [
+                            'id' => Type::nonNull(Type::id()),
+                            'input' => Type::nonNull($driver->input(User::class, [], ['name'])),
+                        ],
+                        'resolve' => function ($root, $args): User {
+                            $user = $this->getEntityManager()->getRepository(User::class)
+                                ->find($args['id']);
+
+                            $user->setName($args['input']['name']);
+                            $this->getEntityManager()->flush();
+
+                            return $user;
+                        },
+                    ],
+                ],
+            ]),
+        ]);
+
+        $query = 'mutation {
+            testInput(id: 1, input: { name: "inputTest" }) {
+                id
+                name
+            }
+        }';
+
+        $result = GraphQL::executeQuery($schema, $query);
+        $output = $result->toArray();
+
+        $this->getEntityManager()->clear();
+        $user = $this->getEntityManager()->getRepository(User::class)
+            ->find(1);
+
+        $this->assertEquals('inputTest', $user->getName());
+        $this->assertEquals(1, $output['data']['testInput']['id']);
+        $this->assertEquals('inputTest', $output['data']['testInput']['name']);
+    }
+
+    public function testInputWithAllFieldsRequired(): void
+    {
+        $config = new Config([
+            'group' => 'InputFactoryTest',
+        ]);
+
+        $driver = new Driver($this->getEntityManager(), $config);
+
+        $schema = new Schema([
+            'mutation' => new ObjectType([
+                'name' => 'mutation',
+                'fields' => [
+                    'testInput' => [
+                        'type' => $driver->type(User::class),
+                        'args' => [
+                            'id' => Type::nonNull(Type::id()),
+                            'input' => Type::nonNull($driver->input(User::class)),
+                        ],
+                        'resolve' => function ($root, $args): User {
+                            $user = $this->getEntityManager()->getRepository(User::class)
+                                ->find($args['id']);
+
+                            $user->setName($args['input']['name']);
+                            $this->getEntityManager()->flush();
+
+                            return $user;
+                        },
+                    ],
+                ],
+            ]),
+        ]);
+
+        $query = 'mutation {
+            testInput(id: 1, input: { name: "inputTest" email: "email" password: "password", id: 1}) {
                 id
                 name
             }
