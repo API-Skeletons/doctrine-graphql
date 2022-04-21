97e7f97ef817cdc9e74fcdd7968a277d2a94f87f
Tom H Anderson <tom.h.anderson@gmail.com>
2022-04-21 02:36:41 -0600

    Throw exception when an identifier is requested as an input

--- src/Input/InputFactory.php
+++ src/Input/InputFactory.php
@@ -8,6 +8,7 @@ use ApiSkeletons\Doctrine\GraphQL\AbstractContainer;
 use ApiSkeletons\Doctrine\GraphQL\Metadata\Metadata;
 use ApiSkeletons\Doctrine\GraphQL\Type\TypeManager;
 use Doctrine\ORM\EntityManager;
+use Exception;
 use GraphQL\Type\Definition\InputObjectType;
 use GraphQL\Type\Definition\Type;
 
@@ -48,26 +49,42 @@ class InputFactory extends AbstractContainer
                     /**
                      * Do not include identifiers as input.  In the majority of cases there will be
                      * no reason to set or update an identifier.  For the case where an identifier
-                     * should be set or updated this facotry is not the correct solution.
+                     * should be set or updated, this facotry is not the correct solution.
                      */
-                    if ($this->entityManager->getClassMetadata($id)->isIdentifier($fieldName)) {
-                        continue;
-                    }
-
                     if ($optionalFields) {
                         // Include field as optional
                         if (in_array($fieldName, $optionalFields) || $optionalFields === ['*']) {
+                            if ($optionalFields === ['*'] && $this->entityManager->getClassMetadata($id)->isIdentifier($fieldName)) {
+                                continue;
+                            }
+
+                            if ($this->entityManager->getClassMetadata($id)->isIdentifier($fieldName)) {
+                                throw new Exception('Identifier ' . $fieldName . ' is an invalid input.');
+                            }
+
                             $fields[$fieldName]['description'] = $targetEntity->getMetadataConfig()['fields'][$fieldName]['description'];
                             $fields[$fieldName]['type']        = $this->typeManager->get($targetEntity->getMetadataConfig()['fields'][$fieldName]['type']);
                         }
                     } elseif ($requiredFields) {
                         // Include fields as required
                         if (in_array($fieldName, $requiredFields) || $requiredFields === ['*']) {
+                            if ($requiredFields === ['*'] && $this->entityManager->getClassMetadata($id)->isIdentifier($fieldName)) {
+                                continue;
+                            }
+
+                            if ($this->entityManager->getClassMetadata($id)->isIdentifier($fieldName)) {
+                                throw new Exception('Identifier ' . $fieldName . ' is an invalid input.');
+                            }
+
                             $fields[$fieldName]['description'] = $targetEntity->getMetadataConfig()['fields'][$fieldName]['description'];
                             $fields[$fieldName]['type']        = Type::nonNull($this->typeManager->get($targetEntity->getMetadataConfig()['fields'][$fieldName]['type']));
                         }
                     } else {
                         // All fields are required
+                        if ($this->entityManager->getClassMetadata($id)->isIdentifier($fieldName)) {
+                            continue;
+                        }
+
                         $fields[$fieldName]['description'] = $targetEntity->getMetadataConfig()['fields'][$fieldName]['description'];
                         $fields[$fieldName]['type']        = Type::nonNull($this->typeManager->get($targetEntity->getMetadataConfig()['fields'][$fieldName]['type']));
                     }
