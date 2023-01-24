ecacb27596aa35732f5b87da9c1e56f2fa2a7773
Tom H Anderson <tom.h.anderson@gmail.com>
2023-01-23 21:26:12 -0700

    Give meaningful prefixes to uniqid names

--- src/Type/Pagination.php
+++ src/Type/Pagination.php
@@ -14,7 +14,7 @@ class Pagination extends InputObjectType
     public function __construct()
     {
         $configuration = [
-            'name' => 'a' . uniqid(), // must start with a letter
+            'name' => 'Pagination_' . uniqid(), // must start with a letter
             'description' => 'Pagination fields for the GraphQL Complete Connection Model',
             'fields' => [
                 'first' => [
