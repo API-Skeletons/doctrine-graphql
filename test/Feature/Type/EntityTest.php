1a817090f307f40d53bf3b92f870b7b28bd640e9
Tom H Anderson <tom.h.anderson@gmail.com>
2023-06-01 23:58:16 -0600

    Added sortFields config option

--- test/Feature/Type/EntityTest.php
+++ test/Feature/Type/EntityTest.php
@@ -6,6 +6,7 @@ namespace ApiSkeletonsTest\Doctrine\GraphQL\Feature\Type;
 
 use ApiSkeletons\Doctrine\GraphQL\Config;
 use ApiSkeletons\Doctrine\GraphQL\Driver;
+use ApiSkeletons\Doctrine\GraphQL\Event\EntityDefinition;
 use ApiSkeletons\Doctrine\GraphQL\Hydrator\Strategy\AssociationDefault;
 use ApiSkeletons\Doctrine\GraphQL\Hydrator\Strategy\FieldDefault;
 use ApiSkeletons\Doctrine\GraphQL\Hydrator\Strategy\ToInteger;
@@ -13,6 +14,12 @@ use ApiSkeletons\Doctrine\GraphQL\Type\Entity;
 use ApiSkeletons\Doctrine\GraphQL\Type\TypeManager;
 use ApiSkeletonsTest\Doctrine\GraphQL\AbstractTest;
 use ApiSkeletonsTest\Doctrine\GraphQL\Entity\Recording;
+use ApiSkeletonsTest\Doctrine\GraphQL\Entity\User;
+use ArrayObject;
+use League\Event\EventDispatcher;
+
+use function array_keys;
+use function sort;
 
 class EntityTest extends AbstractTest
 {
@@ -57,5 +64,34 @@ class EntityTest extends AbstractTest
         $this->assertEquals('Entity Test Users', $metadata['fields']['users']['description']);
 
         $this->assertEquals('entitytestrecording_entityTest', $metadata['typeName']);
+    }
+
+    public function testSortFields(): void
+    {
+        $unsortedFields           = new ArrayObject();
+        $unsortedFields['fields'] = [];
+
+        $config = new Config(['sortFields' => true]);
+
+        $driver = new Driver($this->getEntityManager(), $config);
+
+        // Fields are only sorted after this event is fired
+        $driver->get(EventDispatcher::class)->subscribeTo(
+            User::class . '.definition',
+            static function (EntityDefinition $event) use ($unsortedFields): void {
+                $fields = $event->getDefinition()['fields']();
+
+                $unsortedFields['fields'] = array_keys($fields);
+            },
+        );
+
+        $graphQLType  = $driver->get(TypeManager::class)
+            ->build(Entity::class, User::class)();
+        $fields       = array_keys($graphQLType->getFields());
+        $fieldsSorted = $fields;
+        sort($fieldsSorted);
+
+        $this->assertNotEquals(array_keys($unsortedFields['fields']), $fields);
+        $this->assertEquals($fields, $fieldsSorted);
     }
 }
