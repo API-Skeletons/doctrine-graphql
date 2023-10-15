3f7efe86aa10c6c1fb4bbdc8431f92b4f54fb543
Tom H Anderson <tom.h.anderson@gmail.com>
2023-10-15 04:04:11 -0600

    Upgraded theme and added to conf.py

--- conf.py
+++ conf.py
@@ -1,9 +1,12 @@
 project = 'doctrine-graphql'
 author = 'Tom H Anderson <tom.h.anderson@gmail.com>'
-copyright = '2022 API Skeletons <contact@apiskeletons.com>'
+copyright = '2023 API Skeletons <contact@apiskeletons.com>'
 master_doc = 'index'
 show_sphinx = True
 commit = True
 last_updated = True
 pygments_style = 'sphinx'
+extensions = [
+    'sphinx_rtd_theme',
+]
 html_theme = 'sphinx_rtd_theme'
